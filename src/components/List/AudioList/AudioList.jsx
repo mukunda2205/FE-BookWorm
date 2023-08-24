2877956f271d2a10e4c44a266b08d6461f7757bf
Mukunda Chaudhari <mukundac15@gmail.com>
2023-08-25 01:18:18 +0530

    audioList

---
+++ src/components/List/AudioList/AudioList.jsx
@@ -0,0 +1,95 @@
+import React, { useEffect, useState } from 'react';
+import './AudioList.css'; // Import your custom CSS file
+
+function AudioList() {
+  const [list, setList] = useState([
+    {
+      "id": 1,
+      "productName": "eBook",
+      "price": 0.0,
+      "isRentable": true,
+      "isLibrary": true,
+      "rentPerDay": 0.0,
+      "minRentDays": 0.0
+    }
+  ]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/products/getByType/1")
+      .then(res => res.json())
+      .then(result => {
+        console.log("Result "+result)
+        setList(result); 
+      })
+      .catch(error => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  function addToCart(productId, type) {
+    const customerId = 1;
+
+    fetch("http://localhost:8080/api/cart/addtocart", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization':'Bearer '+localStorage.getItem("Token")
+      },
+      body: JSON.stringify({
+        productId: productId,
+        customerId: customerId,
+        type: type
+      })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log("Success", data);
+      })
+      .catch(error => {
+        console.error("Error", error);
+      });
+  }
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        {list.map((listing, index) => (
+          <div
+            key={index}
+            className='col-md-4 col-sm-6 audio-card' // Apply the "audio-card" class
+          >
+            <div className='card' style={{ cursor: 'pointer' }}>
+              <img
+                style={{ height: '200px', objectFit: 'cover', borderTop: '10px solid white' }}
+                src="Images/janko-ferlic-sfL_QOnmy00-unsplash.jpg"
+                alt={listing.name}
+                className='card-img-top'
+              />
+              <div className='card-body'>
+                <h5 className='card-title'>{listing.id}</h5>
+                <h5 className='card-title'>{listing.productName}</h5>
+                <p className='card-text'>{listing.price}</p>
+                <div className="d-flex justify-content-between">
+                  <button
+                    className='btn btn-primary'
+                    onClick={() => addToCart(listing.id, "buy")}
+                  >
+                    Buy
+                  </button>
+                  <button
+                    className='btn btn-primary'
+                    onClick={() => addToCart(listing.id, "rent")}
+                  >
+                    Rent
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default AudioList;
